c5e7d49208a9bee0bfd9fc48a3a6fa9e08c747ef
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-12-12 10:42:47 -0300

    usando file loader para images

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import image from "./asset/anthony-tran-9vA3h6uzspU-unsplash.jpg";
+import "./style.css";
+
+const box = document.getElementById("box");
+box.innerHTML = "Hello World";
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const { Hash } = require("crypto");
+const path = require("path");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: "[hash].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+          [
+            "svgo",
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+};
